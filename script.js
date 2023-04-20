ccc34bf03f7c0946292fb684dec4de705de3c658
Timmy Chen <chenx343@gmail.com>
2023-04-19 23:28:21 -0400

    added up to filter

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.querySelector(".btn-clear")
+const itemFilter = document.getElementById("filter");
+const items = itemList.querySelectorAll("li");
 
 function addItem(e) {
     e.preventDefault();
     const newItem = itemInput.value;
 
 // Validate Input
-    if (newItem.value === "") {
+    if (newItem === "") {
         alert("Please enter an item");
         return;
     };
@@ -22,6 +25,37 @@ function addItem(e) {
     list.appendChild(li);
 
     itemInput.value = "";
+    checkUI();
+};
+
+function removeItem(e) {
+    if (e.target.classList.contains("fa-xmark")) {
+        e.target.parentElement.parentElement.remove();
+    }
+    checkUI();
+};
+
+function removeAllItems(e) {
+    // const items = itemList.querySelectorAll("li");
+    // items.forEach((item) => {
+    //     item.remove();
+    // });
+
+    while (itemList.firstChild){
+        itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+};
+
+function checkUI() {
+    const items = itemList.querySelectorAll("li"); 
+    if (items.length === 0) {
+        clearBtn.style.display = "none";
+        itemFilter.style.display = "none";
+    } else {
+        clearBtn.style.display = "block";
+        itemFilter.style.display = "block";
+    }
 };
 
 function createBtn(classes) {
@@ -38,5 +72,26 @@ function createIcon(classes) {
     return icon;
 };
 
+function filterItems(e) {
+    const text = e.target.value.toLowerCase();
+    const items = itemList.querySelectorAll("li");
+    console.log(text);
+
+    items.forEach((item) => {
+        if (item.firstChild.textContent.toLowerCase().includes(text) === false) {
+            item.style.display = "none";
+        }
+        else {
+            item.style.display = "flex";
+        }
+    }) 
+
+}
+
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", removeAllItems);
+itemFilter.addEventListener("input", filterItems);
+
+checkUI();
